588cc68d07caee9b7ba659524e530a29359954cc
lucaskab <lucaslfurini@gmail.com>
2024-04-01 15:10:27 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Lucas Lopes Furini on 01.04.24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
